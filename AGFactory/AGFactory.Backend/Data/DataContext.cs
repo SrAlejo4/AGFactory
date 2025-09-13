04a122be2ced8df3d350f1070c6323a38e8d2fb5
SrAlejo4 <alejandrogarci087@gmail.com>
2025-09-13 09:36:28 -0500

    Employee entity and DataBase

---
+++ AGFactory/AGFactory.Backend/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using AGFactory.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection.Emit;
+
+namespace AGFactory.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
