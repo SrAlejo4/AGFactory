e0796ae3d7ebb03f821f24c0d7fc2d2ff372efa1
SrAlejo4 <alejandrogarci087@gmail.com>
2025-09-14 18:50:37 -0500

    Update SeederDb

--- AGFactory/AGFactory.Backend/Data/SeedDb.cs
+++ AGFactory/AGFactory.Backend/Data/SeedDb.cs
@@ -25,6 +25,13 @@ public class SeedDb
             _context.Employees.Add(new Employee { FirstName = "Pablo", LastName = "Garcia", IsActive = true, HireDate = DateTime.Now, Salary = 1250000 });
             _context.Employees.Add(new Employee { FirstName = "Martin", LastName = "Sanchez", IsActive = true, HireDate = DateTime.Now, Salary = 2560000 });
             _context.Employees.Add(new Employee { FirstName = "Abelardo", LastName = "Gomez", IsActive = true, HireDate = DateTime.Now, Salary = 1423000 });
+            _context.Employees.Add(new Employee { FirstName = "Juliana", LastName = "Morales", IsActive = true, HireDate = DateTime.Now, Salary = 1780000 });
+            _context.Employees.Add(new Employee { FirstName = "Camilo", LastName = "Torres", IsActive = true, HireDate = DateTime.Now, Salary = 2985000 });
+            _context.Employees.Add(new Employee { FirstName = "Valentina", LastName = "Rios", IsActive = true, HireDate = DateTime.Now, Salary = 2150000 });
+            _context.Employees.Add(new Employee { FirstName = "Andres", LastName = "Lopez", IsActive = true, HireDate = DateTime.Now, Salary = 1345000 });
+            _context.Employees.Add(new Employee { FirstName = "Carolina", LastName = "Martinez", IsActive = true, HireDate = DateTime.Now, Salary = 2890000 });
+            _context.Employees.Add(new Employee { FirstName = "Jorge", LastName = "Fernandez", IsActive = true, HireDate = DateTime.Now, Salary = 1675000 });
+            _context.Employees.Add(new Employee { FirstName = "Natalia", LastName = "Castro", IsActive = true, HireDate = DateTime.Now, Salary = 2420000 });
         }
         await _context.SaveChangesAsync();
     }
