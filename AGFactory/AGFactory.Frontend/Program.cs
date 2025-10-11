4c9f783cefea5765af379611e4dad17098a1659f
SrAlejo4 <alejandrogarci087@gmail.com>
2025-10-10 21:07:22 -0500

    Preparing frontend for consuming backend

---
+++ AGFactory/AGFactory.Frontend/Program.cs
@@ -0,0 +1,34 @@
+using MudBlazor.Services;
+using AGFactory.Frontend.Components;
+using AGFactory.Frontend.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7108") });
+builder.Services.AddScoped<IRepository, Repository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
